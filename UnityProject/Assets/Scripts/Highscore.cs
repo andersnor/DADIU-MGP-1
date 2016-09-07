799546b2adda2314135d4c406e92c8656e16ae63
Kenneth Jürgensen <gnurgen@gmail.com>
2016-09-07 16:16:23 +0200

    Implemented health tracking and scoring.

---
+++ UnityProject/Assets/Scripts/Highscore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Highscore : MonoBehaviour {
+
+    public float beg;
+
+	// Use this for initialization
+	void Start (){
+        beg = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public float getScore()
+    {
+        return Time.time - beg;
+    }
+}
