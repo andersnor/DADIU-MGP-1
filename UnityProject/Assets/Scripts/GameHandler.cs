e022f6f6481901a8c5e1f3be4cbb81fcf1d6cf63
Daniel Svejstrup Christensen <fusion_link@hotmail.com>
2016-09-07 14:29:07 +0200

    Init gamehandler

---
+++ UnityProject/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameHandler : MonoBehaviour {
+
+    private static GameHandler current;
+
+    public static GameHandler instance{
+        get
+        {
+            if (!current)
+            {
+                current = FindObjectOfType(typeof(GameHandler)) as GameHandler;
+                if (!current)
+                    Debug.LogError("There has to be a GameHandler in the scene.");
+            }
+            return current;
+        }
+    }
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
