58b02d69233a32c9dc0461fb6e73dd86bf5c9b8e
Mette J <mette9jakobsen@hotmail.com>
2016-09-07 16:01:46 +0200

    Gyroscope: moving with translation

---
+++ Assets/Gyroscope.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gyroscope : MonoBehaviour {
+    public GameObject player;
+    private float movementSpeed = 52.0f;
+
+    void Start () {
+        Input.gyro.enabled = true;
+    }
+	
+	
+	void Update () {
+        player.transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
+    }
+    public void MoveForward() {
+        transform.position += transform.forward * Time.deltaTime * movementSpeed;
+    }
+}
