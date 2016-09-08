7b30079ba3071bb38e524502b50dfb9031797098
Anders Noergaard <anders.n89@gmail.com>
2016-09-08 12:18:34 +0200

    Updated game handler

--- UnityProject/Assets/Scripts/GameHandler.cs
+++ UnityProject/Assets/Scripts/GameHandler.cs
@@ -66,7 +66,8 @@ public class GameHandler{
 
     public void TriggerGhostStep()
     {
-        OnGhostStep();
+        if(OnGhostStep != null)
+            OnGhostStep();
     }
 
     public void TriggerGhostAttack()
