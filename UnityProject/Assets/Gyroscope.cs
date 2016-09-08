2977e8ea1eeefc4ad13eea2b5c6b81680c82086b
Mette J <mette9jakobsen@hotmail.com>
2016-09-08 10:52:03 +0200

    VR Movement

--- UnityProject/Assets/Gyroscope.cs
+++ UnityProject/Assets/Gyroscope.cs
@@ -3,17 +3,33 @@ using System.Collections;
 
 public class Gyroscope : MonoBehaviour {
     public GameObject player;
-    private float movementSpeed = 52.0f;
+    private Rigidbody rid;
+
+    private bool isMoving = false;
+    private float movementSpeed = 150.0f;
+    private float drag = 0.9f;
 
     void Start () {
+        rid = GetComponent<Rigidbody>();
         Input.gyro.enabled = true;
     }
 	
 	
-	void Update () {
+	void FixedUpdate () {
         player.transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
-    }
-    public void MoveForward() {
-        transform.position += transform.forward * Time.deltaTime * movementSpeed;
+        //Check for touch input and set movement to true
+        if (Input.touchCount != 0)
+            isMoving = true;
+        else
+            isMoving = false;
+        
+        //If moving move towards facing direction
+        if (isMoving == true)
+        {
+            rid.velocity = transform.forward * movementSpeed* Time.deltaTime;
+        }
+        else {
+            rid.velocity = rid.velocity * (1-drag * Time.deltaTime);
+        }
     }
 }
