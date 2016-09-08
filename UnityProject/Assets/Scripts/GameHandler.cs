725f2b1c93202f98d596a1c3633364afe1473ea2
Daniel Svejstrup Christensen <fusion_link@hotmail.com>
2016-09-08 09:21:36 +0200

    Game handler events and triggers

--- UnityProject/Assets/Scripts/GameHandler.cs
+++ UnityProject/Assets/Scripts/GameHandler.cs
@@ -14,14 +14,17 @@ public class GameHandler{
     public delegate void MusicBoxRewind();
     public event MusicBoxRewind OnMusicBoxRewind;
 
+    public delegate void MusicBoxRewinded();
+    public event MusicBoxRewinded OnMusicBoxRewinded;
+
     public delegate void MusicBoxTimeout();
     public event MusicBoxTimeout OnMusicBoxTimeout;
 
-    public delegate void GhostStep();
-    public event GhostStep OnGhostStep;
-
     public delegate void PlayerStep();
     public event PlayerStep OnPlayerStep;
+
+    public delegate void GhostStep();
+    public event GhostStep OnGhostStep;
 
     public delegate void GhostAttack();
     public event GhostAttack OnGhostAttack;
@@ -40,4 +43,35 @@ public class GameHandler{
         current = null;
         SceneManager.LoadScene(0);
     }
+
+    public void TriggerMusicBoxRewind()
+    {
+        OnMusicBoxRewind();
+    }
+
+    public void TriggerMusicBoxRewinded()
+    {
+        OnMusicBoxRewinded();
+    }
+
+    public void TriggerMusicBoxTimeout()
+    {
+        OnMusicBoxTimeout();
+    }
+
+    public void TriggerPlayerStep()
+    {
+        OnPlayerStep();
+    }
+
+    public void TriggerGhostStep()
+    {
+        OnGhostStep();
+    }
+
+    public void TriggerGhostAttack()
+    {
+        OnGhostAttack();
+    }
+
 }
