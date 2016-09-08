91aaa7d143e841c318349b93bd8d8eb903a1e3f9
Nicolai Skovhus <nskovh11@student.aau.dk>
2016-09-08 10:32:11 +0200

    GameMenu (load gameScene)

---
+++ UnityProject/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour {
+    public int gameSceneIndex;
+
+    void Start()
+    {
+        if (SceneManager.GetSceneAt(gameSceneIndex) == null || SceneManager.GetSceneAt(gameSceneIndex) == SceneManager.GetSceneAt(Application.loadedLevel))
+            print("gameSceneIndex appears to incorrectly set - Assing gameSceneIndex variable");
+    }
+    public void StartGame()
+    {
+        if (SceneManager.GetSceneAt(gameSceneIndex) != null)
+            SceneManager.LoadScene(gameSceneIndex);
+    }
+}
