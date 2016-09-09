9ecb2b8517c76457f8a42feb4c0b9953e8747b38
Kenneth Jürgensen <gnurgen@gmail.com>
2016-09-09 15:32:05 +0200

    Initial unit testing

---
+++ UnityProject/Assets/Editor/Test1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class Test1 {
+
+	[Test]
+	public void PlayerExists()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Assert.True(player != null);
+	}
+
+    [Test]
+    public void MusicBoxExists()
+    {
+        GameObject music = GameObject.FindGameObjectWithTag("MusicBox");
+    }
+}
---
+++ UnityProject/Assets/Scripts/Builder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class Builder{
+
+    static void PerformBuild()
+    {
+        string[] scenes = { "Assets/Scenes/Kenneth Test Scene" };
+        BuildPipeline.BuildPlayer(scenes, "PlayerBuild", BuildTarget.Android,BuildOptions.None);
+    }
+	
+}
