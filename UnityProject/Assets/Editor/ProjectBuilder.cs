51f7fe00ac33a8b7cc8de853072f77b3bd0270f0
Kenneth Jürgensen <gnurgen@gmail.com>
2016-09-12 11:00:45 +0200

    Implemented within range spawn, but not tested yet.

---
+++ UnityProject/Assets/Editor/ProjectBuilder.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ProjectBuilder{
+
+	public static void BuildProject()
+    {
+        string[] scenes = { "Assets/Scenes/StartScene.unity", "Assets/Scenes/Gray box.unity", "Assets/Scenes/EndSceneMusic", "Assets/Scenes/EndSceneGhost" };
+        BuildPipeline.BuildPlayer(scenes, "TestBuilds/FirstBuild.apk", BuildTarget.Android, BuildOptions.None);
+    }
+}
