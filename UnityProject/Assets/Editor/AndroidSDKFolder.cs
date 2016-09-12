4d8614311162facf7ce54df713050e4e8c6d57b1
Kenneth Jürgensen <gnurgen@gmail.com>
2016-09-12 12:51:08 +0200

    Trying to build

---
+++ UnityProject/Assets/Editor/AndroidSDKFolder.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+
+public static class AndroidSDKFolder{
+
+    public static string Path
+        {
+            get { return EditorPrefs.GetString("C:\\Users\\dadiu\\AppData\\Local\\Android\\sdk"); }
+            set { EditorPrefs.SetString("C:\\Users\\dadiu\\AppData\\Local\\Android\\sdk", value); }
+        }
+}
+
--- UnityProject/Assets/Editor/ProjectBuilder.cs
+++ UnityProject/Assets/Editor/ProjectBuilder.cs
@@ -7,6 +7,6 @@ public class ProjectBuilder{
 	public static void BuildProject()
     {
         string[] scenes = { "Assets/Scenes/StartScene.unity", "Assets/Scenes/Gray box.unity", "Assets/Scenes/EndSceneMusic", "Assets/Scenes/EndSceneGhost" };
-        BuildPipeline.BuildPlayer(scenes, "TestBuilds/FirstBuild.apk", BuildTarget.Android, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes, "TestBuilds/FirstBuild.apk", BuildTarget.StandaloneWindows, BuildOptions.None);
     }
 }
